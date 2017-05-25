f76e32ff627901fe45798f5d0e0cf48dd956d213
Konstantin Snegov <ksnegov@ozon.ru>
2017-05-25 09:47:42 +0300

    Init commit

---
+++ SearchDemo/Application/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchDemo
+//
+//  Created by Konstantin Snegov on 25/05/2017.
+//  Copyright © 2017 Konstantin Snegov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchDemo/Application/AppDelegate/AppDelegate.m
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.m
+//  SearchDemo
+//
+//  Created by Konstantin Snegov on 25/05/2017.
+//  Copyright © 2017 Konstantin Snegov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+@end
